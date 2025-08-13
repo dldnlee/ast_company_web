0ee8e4f0de67005c75803d00695ce0bc874d5997
dldnlee <sungeun8877@gmail.com>
2025-08-13 22:22:50 +0900

    💄 style(portfolio): adjust portfolio section layout
    - "전체보기" 버튼에 margin-top 추가하여 레이아웃 개선

--- app/sections/PortfolioSection.tsx
+++ app/sections/PortfolioSection.tsx
@@ -8,7 +8,7 @@ export function PortfolioSection() {
         <section className="w-full flex flex-col items-center justify-center py-16">
             <h2 className="text-3xl font-bold">OUR WORKS</h2>
             <AnimatedTabs />
-            <Link href="/videos" className='border px-5 py-2 flex justify-center items-center group hover:text-purple-600'>
+            <Link href="/videos" className='border mt-5 px-5 py-2 flex justify-center items-center group hover:text-purple-600'>
                 <p>전체보기</p>
                 <FaArrowRight className='inline ml-2 group-hover:translate-x-2 transition-all' />
             </Link>
